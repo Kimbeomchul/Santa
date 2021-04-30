3d0cc5dad1ef6405478db911f1d0a360cf7ee658
yjk <na86421@naver.com>
2021-04-30 14:41:51 +0900

    initial commit & complete project setting

---
+++ bakends/hiking/hiking/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from hiking.users.models import User
+from hiking.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
