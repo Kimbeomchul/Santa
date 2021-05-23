281246d3bf5e1572b59e6e134392e0d894cf8d7e
yjk <na86421@naver.com>
2021-05-23 16:10:39 +0900

    userserializer 필드 추가

--- bakends/hiking/hiking/users/api/serializers.py
+++ bakends/hiking/hiking/users/api/serializers.py
@@ -1,19 +1,32 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from allauth.socialaccount.models import SocialAccount
 from hiking.users.models import Board
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    provider = serializers.SerializerMethodField('get_provider')
+    photo = serializers.SerializerMethodField('get_photo')
+
     class Meta:
         model = User
-        fields = ["username", "name", "url"]
+        fields = ["id", "username", "name", "email", "provider", "photo", "url"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
         }
+
+    def get_provider(self, obj):
+        social_account = SocialAccount.objects.get(user_id=obj.id)
+        return social_account.provider
+
+    def get_photo(self, obj):
+        social_account = SocialAccount.objects.get(user_id=obj.id)
+        print(social_account.extra_data['picture'])
+        return social_account.extra_data['picture']
 
 
 class BoardSerializer(serializers.ModelSerializer):
